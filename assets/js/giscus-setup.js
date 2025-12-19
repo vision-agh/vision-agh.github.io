d8737b9e2676affa0b78a07303a59961677d3348
harpun11 <wasala@agh.edu.pl>
2025-12-19 17:15:28 +0000

    Deploying to gh-pages from @ vision-agh/vision-agh.github.io@b61d23ee8d47877cdc8c043307475e53a8fa338c 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "vision-agh/www",
+    "data-repo": "vision-agh/vision-agh.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
